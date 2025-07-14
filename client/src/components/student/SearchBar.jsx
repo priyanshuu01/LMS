1116ff1449bad84c4d157cb3c0a14b195d82cfaf
priyanshuu01 <priyanshu.yadav2_cs22@gla.ac.in>
2025-07-14 08:47:24 +0530

    Add or update project files

---
+++ client/src/components/student/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const SearchBar = () => {
+  return (
+      <form className='max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded'>
+        <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10 px-3' />
+        <input type="text" placeholder='search for courses' className='w-full h-full outline-none text-gray-500/80'/>
+        <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>Search</button>
+      </form>
+    
+  )
+}
+
+export default SearchBar
